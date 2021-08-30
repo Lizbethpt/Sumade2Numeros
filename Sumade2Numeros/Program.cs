ba783474aea475fd16e3863df35800ed80412060
Hector Peña <Hector Pea@LAPTOP-B0UDD5GC>
2021-08-30 09:01:39 -0500

    Add project files.

---
+++ Sumade2Numeros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sumade2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de 2 Numeros!");
+            // int sumar
+            int a = 10;
+            int b = 5;
+            int c = (a + b);
+            Console.WriteLine(c);
+        }
+    }
+}
